d20d8485f99ab2ad647b27015c75d98288b90bfa
seohay-1ae <mokash1ae@gmail.com>
2025-06-16 17:50:10 +0900

    myavatar 수정

--- src/main/java/com/project/travelquest/avatar/controller/MyAvatarController.java
+++ src/main/java/com/project/travelquest/avatar/controller/MyAvatarController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MyAvatarController {
     @GetMapping("/myavatarTest")
     public String showPage() {
-        return "myAvatar"; // /WEB-INF/views/yourPage.jsp 로 이동
+        return "mypage/myAvatar"; // /WEB-INF/views/yourPage.jsp 로 이동
     }
 }
